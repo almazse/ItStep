f2abc7bd0d4f0f523456c0c51c47517758acf030
almazse <52276649+almazse@users.noreply.github.com>
2021-06-08 19:28:18 +0300

    HW: update game

--- ITS_18/HW/controllers/game.py
+++ ITS_18/HW/controllers/game.py
@@ -1,23 +1,33 @@
-from ITS_18.HW.views.tkinter_view import TkinterView
-from ITS_18.HW.models.cells import Cells
+from views.tkinter_view import TkinterView
+from models.cells import Cells
+from models.model import Data
 import tkinter as tk
-import time
 
 
 class LifeGame:
     def __init__(self):
-        self.cells_number_by_height = 30
-        self.cells_number_by_width = 40
-        self.cell_size = 20
+        self.root = tk.Tk()
+        self.data = Data()
+        self.view = TkinterView(self.root)
         self.cells = Cells()
-
-
-        self.view = TkinterView(self.cells_number_by_height, self.cells_number_by_width, self.cell_size)
+        self.cells.get_cells()
 
     def start(self):
+        self.view.create_canvas(self.data.cells_number_by_height, self.data.cell_size, self.data.cells_number_by_width)
         self._draw_grid()
+        self.view.create_button()
+        self.root.mainloop()
 
     def _draw_grid(self):
-        for i in range(0, self.cell_size * self.cells_number_by_height, self.cell_size):
-            self.view.create_line_vertical(i)
+        for i in range(0, self.data.cell_size * self.data.cells_number_by_height, self.data.cell_size):
+            self.view.create_line_vertical(i, self.data.cells_number_by_width, self.data.cell_size)
+        for i in range(0, self.data.cell_size * self.data.cells_number_by_width, self.data.cell_size):
+            self.view.create_line_horizontal(i, self.data.cells_number_by_height, self.data.cell_size)
 
+    def click(self, e, canvas):
+        global sums, cells
+        if e.widget != canvas:
+            return
+        i, j = e.y // self.data.cell_size, e.x // self.data.cell_size
+        self.cells[i][j] = 1
+        # generation()
--- ITS_18/HW/views/tkinter_view.py
+++ ITS_18/HW/views/tkinter_view.py
@@ -1,30 +1,35 @@
 import tkinter as tk
+from controllers.game import LifeGame
 
 
 class TkinterView:
-    def __init__(self, cells_number_by_height, cells_number_by_width, cell_size):
-        self.cells_number_by_height = cells_number_by_height
-        self.cells_number_by_width = cells_number_by_width
-        self.cell_size = cell_size
-        self.root = tk.Tk()
+    def __init__(self, root):
+        self.root = root
+        self.cells_number_by_height = 30
+        self.cells_number_by_width = 40
+        self.cell_size = 20
+        self.canvas = tk.Canvas()
 
-
-
+    def create_canvas(self, cells_number_by_height, cell_size, cells_number_by_width):
         self.canvas = tk.Canvas(self.root,
-                           height=cells_number_by_height * cell_size,
-                           width=cells_number_by_width * cell_size,
-                           bg='lightgrey')
+                                height=cells_number_by_height * cell_size,
+                                width=cells_number_by_width * cell_size,
+                                bg='lightgrey')
         self.canvas.pack(padx=5, pady=5)
 
+    def create_button(self):
         btn_next_gen = tk.Button(self.root, text='Next generation')
         btn_next_gen.pack()
 
         btn_next_gen_100 = tk.Button(self.root, text='100 Next generation')
         btn_next_gen_100.pack()
 
-    def create_line_vertical(self, i):
-        self.canvas.create_line(0, i, self.cells_number_by_width*self.cell_size, i)
-        self.mainloop()
+    def create_line_vertical(self, i, cells_number_by_width, cell_size):
+        self.canvas.create_line(0, i, cells_number_by_width * cell_size, i)
+
+    def create_line_horizontal(self, i, cells_number_by_height, cell_size):
+        self.canvas.create_line(i, 0, i, cells_number_by_height * cell_size)
+
 
 
 
