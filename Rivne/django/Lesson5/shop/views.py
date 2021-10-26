0f97780aea03ea22df38601947d1828fa5e3678a
Kirill <961451@ukr.net>
2021-10-26 16:39:02 +0300

    bradcrumb, pagination
    add functional breadcrumb and pagination products in store

--- Rivne/django/Lesson5/shop/views.py
+++ Rivne/django/Lesson5/shop/views.py
@@ -1,45 +1,47 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Brand, Category
-from cart.forms import CartAddProductForm
-from django.conf import settings
+from django.core.paginator import Paginator
 
 
 def homepage(request):
-    return render(request, 'pages/index.html')
+	return render(request, 'pages/index.html')
 
 
 def store(request, category_slug=None):
-    category = None
-    categories = Category.objects.all()
+	category = None
+	categories = Category.objects.all()
 
-    category_id_list = []
-    for i in categories:
-        if request.POST.get(f"category-{i.id}", False) == 'on':
-            category_id_list.append(i.id)
+	category_id_list = []
+	for i in categories:
+		if request.POST.get(f"category-{i.id}", False) == 'on':
+			category_id_list.append(i.id)
 
+	brand = None
+	brands = Brand.objects.all()
 
-    brand = None
-    brands = Brand.objects.all()
+	if category_id_list:
+		products = Product.objects.filter(available=True, category__in=category_id_list)
+	else:
+		products = Product.objects.filter(available=True)
 
-    if category_id_list:
-        products = Product.objects.filter(available=True, category__in=category_id_list)
-    else:
-        products = Product.objects.filter(available=True)
+	if category_slug:
+		category = get_object_or_404(Category, slug=category_slug)
+		products = Product.objects.filter(category=category)
 
-    if category_slug:
-        category = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.filter(category=category)
+	paginator = Paginator(products, 1)
+	page_number = request.GET.get('page')
+	products = paginator.get_page(page_number)
 
-    return render(request, 'pages/store.html', {'category': category,
-                                                'categories': categories,
-                                                'products': products,
-                                                'category_id_list': category_id_list})
+	return render(request, 'pages/store.html', {'category': category,
+												'categories': categories,
+												'products': products,
+												'category_id_list': category_id_list})
 
 
 def checkout(request):
-    return render(request, 'pages/checkout.html')
+	return render(request, 'pages/checkout.html')
 
 
 def product(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    return render(request, 'pages/product.html', {'product': product})
+	product = get_object_or_404(Product, pk=pk)
+	return render(request, 'pages/product.html', {'product': product})
